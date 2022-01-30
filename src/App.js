9b10822491e05cd66f29044840b287a599332041
Song Ha <songsongha@gmail.com>
2022-01-30 00:28:19 -0700

    Add socket server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from 'react'
 import { Client } from 'boardgame.io/react'
+import { SocketIO } from 'boardgame.io/multiplayer'
 import { TicTacToe } from './Game'
 import { TicTacToeBoard } from './Board'
 
-const App = Client({
+const TicTacToeClient = Client({
   game: TicTacToe,
   board: TicTacToeBoard,
-});
+  multiplayer: SocketIO({ server: 'localhost:8000' })
+})
+
+const App = () => (
+  <div>
+    <TicTacToeClient playerID="0" />
+    <TicTacToeClient playerID="1" />
+  </div>
+)
 
 export default App
 
---
+++ src/server.js
@@ -0,0 +1,10 @@
+// src/server.js
+const { Server, Origins } = require('boardgame.io/server')
+const { TicTacToe } = require('./Game')
+
+const server = Server({
+  games: [TicTacToe],
+  origins: [Origins.LOCALHOST],
+})
+
+server.run(8000)
